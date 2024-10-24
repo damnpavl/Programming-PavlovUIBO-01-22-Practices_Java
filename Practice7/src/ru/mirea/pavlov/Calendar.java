c5c5f18537ee900f81988ff19382b494457e4769
damnpavl <damnpavl0v@icloud.com>
2024-10-24 14:47:17 +0300

    Add files via upload

---
+++ Practice7/src/ru/mirea/pavlov/Calendar.java
@@ -0,0 +1,26 @@
+package ru.mirea.pavlov;
+
+import java.util.GregorianCalendar;
+
+public class Calendar {
+    public static void main(String[] args) {
+        GregorianCalendar cal = new GregorianCalendar();
+
+        int year = cal.get(java.util.Calendar.YEAR);
+        int month = cal.get(java.util.Calendar.MONTH) + 1;
+        int day = cal.get(java.util.Calendar.DAY_OF_MONTH);
+
+        System.out.println("Текущий год: " + year);
+        System.out.println("Текущий месяц: " + month);
+        System.out.println("Текущий день: " + day);
+
+        cal.setTimeInMillis(1234567898765L);
+        year = cal.get(java.util.Calendar.YEAR);
+        month = cal.get(java.util.Calendar.MONTH) + 1;
+        day = cal.get(java.util.Calendar.DAY_OF_MONTH);
+
+        System.out.println("Год для 1234567898765L: " + year);
+        System.out.println("Месяц для 1234567898765L: " + month);
+        System.out.println("День для 1234567898765L: " + day);
+    }
+}
